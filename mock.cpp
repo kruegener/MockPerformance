9d101baa479423ffd07cfacad051d6715da3a6d9
Moritz Krügener <kruegener@users.noreply.github.com>
2019-03-15 15:18:30 +0100

    initial mock commit

---
+++ mock.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Kruegener on 3/15/2019.
+//
+
+#include "mock.h"
+#include <iostream>
+
+float commit_bias = 1.0;
+
+int main(){
+
+#ifdef AVX
+	MMUPS += 1;
+#endif
+
+#ifdef AVX2
+	MMUPS += 2;
+#endif
+
+	MMUPS += commit_bias;
+
+	std::cout << "Simulation speed: " << MMUPS << " Molecule-updates per second." << std::endl;
+
+	return 0;
+
+}
---
+++ mock.h
@@ -0,0 +1,10 @@
+//
+// Created by Kruegener on 3/15/2019.
+//
+
+#ifndef MOCKPERFORMANCE_MOCK_H
+#define MOCKPERFORMANCE_MOCK_H
+
+double MMUPS = 0.0;
+
+#endif //MOCKPERFORMANCE_MOCK_H
